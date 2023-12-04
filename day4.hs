ab0bc0595832f64a6749cfbf111b78bcb9218e4a
Oliver Lomax <oliver@otl.org.uk>
2023-12-04 21:21:49 +0000

    day 4 part 2

--- day4.hs
+++ day4.hs
@@ -6,12 +6,11 @@ testData = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n\
             \Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n\
             \Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11\n"
 
--- data Card = Card {cardNumber :: Int,
---                   winners :: [Int],
---                   actuals :: [Int]} deriving (Show)
 
-parseLine :: String -> (Int, [Int], [Int])
-parseLine ls = (read  (head split1), map read (head split3), map read (last split3))-- head split1 : split2
+type InputLine = (Int, [Int], [Int])
+
+parseLine :: String -> InputLine
+parseLine ls = (read  (head split1), map read (head split3), map read (last split3))
             where split1 = splitOn ": " $ drop 5 ls
                   split2 = splitOn "|" (last split1)
                   split3 = map (filter (/= "") .splitOn " ") split2
@@ -20,13 +19,33 @@ countMatchs :: [Int] -> [Int] -> Int
 countMatchs winners chosen = length $ filter (`elem` winners) chosen
 
 
-points :: (Int, [Int], [Int]) -> Int
+points :: InputLine -> Int
 points (_, a, b) = if winners == 0 then 0 else 2 ^ (winners-1)
       where winners = countMatchs a b
 
 part1 = do
       contents <- readFile "input_files/aoc23-4.txt"
-      -- let inputData = lines contents
       let fullData = map parseLine (lines contents)
       let score = sum $ map points fullData
       return score
+
+cardCounts :: [Int]
+cardCounts = replicate 10 1
+
+updateCardCounts:: [Int] -> Int -> Int -> [Int]
+updateCardCounts xs ncards m = map (+m) (take ncards xs) ++ drop ncards xs
+
+scoreList :: ([Int], [Int])
+scoreList = unzip [(1, countMatchs a b) | (x, a, b) <- map parseLine (lines testData)]
+
+scoreIt:: [Int] -> [Int] -> [Int]
+scoreIt [] [] = []
+scoreIt (count:counts) (win:wins) = count: scoreIt (updateCardCounts counts win count) wins
+
+part2 :: IO Int
+part2 = do
+      contents <- readFile "input_files/aoc23-4.txt"
+      let fullData = map parseLine (lines contents)
+      let scoreList = unzip [(1, countMatchs a b) | (x, a, b) <- fullData]
+      let cards = scoreIt (fst scoreList) (snd scoreList)
+      return (sum cards)
