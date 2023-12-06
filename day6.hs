caac29a1bce0fc84c927c4b7e49639e631d4383c
Oliver Lomax <oliver@otl.org.uk>
2023-12-06 15:43:00 +0000

    minimal code

--- day6.hs
+++ day6.hs
@@ -1,16 +1,7 @@
 module Main where
--- chargeTime :: Float -> Float -> Float
--- chargeTime totalTime distance = (totalTime + sqrt (totalTime^2 - (4 * distance))) / 2
 
--- validTimes :: Float -> Float -> [Int]
 validTimes totalTime distance = filter (\c -> (fromIntegral c * (totalTime - fromIntegral c))>distance ) [1..round totalTime]
 
--- distance c t = c * (t - c)
-
--- isValid d t c = (c * (t - c))>=d
-
--- countallvalid :: [(Float, Float)] -> Int
--- countallvalid :: [(Float, Float)] -> Int
 countallvalid input = product $ map (length . uncurry validTimes) input
 main:: IO ()
 main = do
