76894e008946083ad41fc1f151de1f8b179f9b8f
Oliver Lomax <oliver@otl.org.uk>
2023-12-06 15:45:48 +0000

    With type declarations

--- day6.hs
+++ day6.hs
@@ -1,8 +1,11 @@
 module Main where
 
+validTimes ::  Float -> Float -> [Int]
 validTimes totalTime distance = filter (\c -> (fromIntegral c * (totalTime - fromIntegral c))>distance ) [1..round totalTime]
 
+countallvalid :: [(Float, Float)] -> Int
 countallvalid input = product $ map (length . uncurry validTimes) input
+
 main:: IO ()
 main = do
     print (countallvalid [(61677571, 430103613071150)])
